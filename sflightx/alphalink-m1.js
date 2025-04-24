0fadbb4c4035c8bdeb183d4a728012abcd5d32df
sflightx <spaceflightxploration@gmail.com>
2025-04-24 17:43:03 +0800

    Remove HTML file for AlphaLink mission and add corresponding JavaScript articles with updated content

---
+++ sflightx/alphalink-m1.js
@@ -0,0 +1,34 @@
+const articleContent = [
+    {
+        type: "iframe",
+        src: "https://www.youtube.com/embed/97oERWoNCbQ?si=EkVBWZq8LJ9V1k4D",
+        title: "YouTube video player",
+        frameborder: "0",
+        allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",
+        referrerpolicy: "strict-origin-when-cross-origin",
+        allowfullscreen: "false",
+        style: {
+            width: "100%",
+            aspectRatio: "16/9",
+        }
+    },
+    {
+        type: "div",
+        class: "container",
+        id: "full",
+        children: [
+            {
+                type: "h4",
+                class: "md-typescale-headline-large",
+                content: "Maya Block 4 launched the AlphaLink Mission on June 25, 08:00 UTC at Launch Complex 1A, Bataan Space Center, Philippines which carried Arenz Space's AlphaLink Satellites to LEO."
+            },
+            {
+                type: "p",
+                class: "md-typescale-body-large",
+                content: "This mission, counts as sFlightX's 12th launch attempt and our 11th orbital launch of 2024."
+            }
+        ]
+    }
+];
+
+export default articleContent;
---
+++ sflightx/dss-1.js
@@ -0,0 +1,29 @@
+const articleContent = [
+    {
+        type: "blockquote",
+        class: "twitter-tweet",
+        attributes: {
+            "data-media-max-width": "560"
+        },
+        content: 'Last year, we successfully launched the first Droo Space Station mission (DSS-1) to Low Droo Orbit.<br><br>The booster supporting this mission was expended to meet the highest lift capabilities of the earlier version of the Unity Module. <a href="https://t.co/ijvTaCDBhO">pic.twitter.com/ijvTaCDBhO</a><br>&mdash; sFlightX JNO (@sFlightXJNO) <a href="https://twitter.com/sFlightXJNO/status/1874963849167905140?ref_src=twsrc%5Etfw">January 2, 2025</a>'
+    },
+    {
+        type: "div",
+        class: "container",
+        id: "full",
+        children: [
+            {
+                type: "h4",
+                class: "md-typescale-headline-large",
+                content: "Maya Block 4 launched the AlphaLink Mission on June 25, 08:00 UTC at Launch Complex 1A, Bataan Space Center, Philippines which carried Arenz Space's AlphaLink Satellites to LEO."
+            },
+            {
+                type: "p",
+                class: "md-typescale-body-large",
+                content: "This mission, counts as sFlightX's 12th launch attempt and our 11th orbital launch of 2024."
+            }
+        ]
+    }
+];
+
+export default articleContent;
